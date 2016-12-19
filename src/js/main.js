956475c3c466694dc71ca242de8954b240f553f3
Anton SHulzhenko <shulzhenkomailbox@gmail.com>
2016-12-19 18:25:45 +0200

    added validation file

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,23 @@
 (function($) {
+  var form = $('body').find('form');
+  var inputName = form.find('input[name=name]');
+  var inputEmail = form.find('input[name=email]');
+  var textarea = form.find('textarea[name=textarea]');
+  var formLabel = form.find('.send-form__label');
+
+  form.on('submit', function(e) {
+    if(inputName.val() == "" || (inputEmail.val() == "" || inputEmail.val().indexOf('@') == -1 && inputEmail.val().indexOf('.') == -1) || textarea.val() == "") {
+      
+      alert('error');
+      e.preventDefault();
+    }
+
+    console.log(textarea.val());
+  });
+
+})(jQuery);
+
+(function($) {
   var header = $('.header');
   var goTop = $('.go-top');
   var bodyHeight = $('body').height();
