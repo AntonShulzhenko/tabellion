9ec924be6b2303e610816c87b0db56d49321e2b3
Anton SHulzhenko <shulzhenkomailbox@gmail.com>
2016-12-21 20:12:14 +0200

    started adding animation

--- src/js/main.js
+++ src/js/main.js
@@ -142,3 +142,14 @@ function initMap() {
 		});
   }
 })(jQuery);
+
+var wow = new WOW(
+  {
+    boxClass:        'trigger',
+    animateClass:    'anim',
+    offset:          100,
+    mobile:          false,
+    scrollContainer: null
+  }
+);
+wow.init();
---
+++ src/js/modules/_animation.js
@@ -0,0 +1,10 @@
+var wow = new WOW(
+  {
+    boxClass:        'trigger',
+    animateClass:    'anim',
+    offset:          100,
+    mobile:          false,
+    scrollContainer: null
+  }
+);
+wow.init();
