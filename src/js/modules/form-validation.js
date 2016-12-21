8d7e9dc8fd44cd3d34d4528985cd67992f11bb05
Anton SHulzhenko <shulzhenkomailbox@gmail.com>
2016-12-21 18:39:54 +0200

    added form to modal

--- src/js/modules/form-validation.js
+++ src/js/modules/form-validation.js
@@ -1,43 +1,46 @@
 (function($) {
-  var form       = $('body').find('form');
-  var inputName  = form.find('input[name=name]');
-  var inputEmail = form.find('input[name=email]');
-  var textarea   = form.find('textarea[name=textarea]');
-  var formLabel  = form.find('.send-form__label');
-  var formFields = formLabel.children(':first-child');
-  var emailReg   = /.+@.+\..+/i;
+  var form     = $('body').find('form');
+  var emailReg = /.+@.+\..+/i;
 
-  form.on('submit', function(e) {
-    if(inputName.val() == "" || inputEmail.val() == "" || textarea.val() == "") {
-      inputName.siblings('.send-form__error').fadeIn();
-      inputEmail.siblings('.send-form__error').fadeIn();
-      textarea.siblings('.send-form__error').fadeIn();
-      e.preventDefault();
-    } else if(!emailReg.test(inputEmail.val())) {
-      inputEmail.siblings('.send-form__error').fadeIn();
-      e.preventDefault();
-    }
-  });
+  form.each(function(i) {
+    var inputName  = $(this).find('input[name=name]');
+    var inputEmail = $(this).find('input[name=email]');
+    var textarea   = $(this).find('textarea[name=textarea]');
+    var formLabel  = $(this).find('.send-form__label');
+    var formFields = formLabel.children(':first-child');
 
-  formFields.each(function(i, el) {
-    var self = $(this);
-
-    self.on('focusout', function() {
-      if(self.attr('name') == 'email') {
-        if(!emailReg.test(self.val())) {
-          self.siblings('.send-form__error').fadeIn();
-        }
-      } else {
-        if(self.val() == "") {
-          self.siblings('.send-form__error').fadeIn();
-        }
+    $(this).on('submit', function(e) {
+      if(inputName.val() == "" || inputEmail.val() == "" || textarea.val() == "") {
+        inputName.siblings('.send-form__error').fadeIn();
+        inputEmail.siblings('.send-form__error').fadeIn();
+        textarea.siblings('.send-form__error').fadeIn();
+        e.preventDefault();
+      } else if(!emailReg.test(inputEmail.val())) {
+        inputEmail.siblings('.send-form__error').fadeIn();
+        e.preventDefault();
       }
     });
 
-    self.on('keypress', function() {
-      if(self.val() != "") {
-        self.siblings('.send-form__error').fadeOut();
-      }
+    formFields.each(function(i, el) {
+      var self = $(this);
+
+      self.on('focusout', function() {
+        if(self.attr('name') == 'email') {
+          if(!emailReg.test(self.val())) {
+            self.siblings('.send-form__error').fadeIn();
+          }
+        } else {
+          if(self.val() == "") {
+            self.siblings('.send-form__error').fadeIn();
+          }
+        }
+      });
+
+      self.on('keypress', function() {
+        if(self.val() != "") {
+          self.siblings('.send-form__error').fadeOut();
+        }
+      });
     });
   });
 })(jQuery);
