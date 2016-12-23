1fb009182e64afcd149b3fa038e4d5fa7ab2d7d2
Anton SHulzhenko <shulzhenkomailbox@gmail.com>
2016-12-23 20:01:37 +0200

    added anim libs

--- src/js/modules/_animation.js
+++ src/js/modules/_animation.js
@@ -8,3 +8,38 @@ var wow = new WOW(
   }
 );
 wow.init();
+
+var header = document.querySelector('.header');
+var headerContacts = header.querySelectorAll('.contacts__item');
+var navItems = document.querySelectorAll('.nav__item');
+var logoOverlay = document.querySelector('.header__logo-overlay');
+var logoLine = document.querySelector('.header__logo-line');
+var logoLineWidth = '324px';
+var winWidth = window.innerWidth;
+
+if(winWidth > 1199 && winWidth < 1800) {
+  logoLineWidth = '220px';
+} else if(winWidth > 899 && winWidth < 1200) {
+  logoLineWidth = '100px';
+}
+
+var tlNav = new TimelineMax({
+  ease: Power4.easeOut
+});
+
+var tlHeader = new TimelineMax({
+  ease: Power4.easeOut
+});
+
+Pace.on('hide', function() {
+  tlHeader.from($('.header__logo'), 1, {x: -50});
+
+  tlHeader
+    .fromTo(logoOverlay, 0.5, {width: '100%'}, {width: '0px'})
+    .fromTo(logoLine, 0.5, {width: '0'}, {width: logoLineWidth})
+    .staggerFrom(headerContacts, 0.75, {x: -50, opacity: 0}, 0.25);
+
+  tlHeader.to($('.header__logo'), 1.5, {x: 0});
+
+  tlNav.staggerFrom(navItems, 2, {x: -50, opacity: 0, delay: 0.25}, 0.25);
+});
--- src/js/modules/loader.js
+++ src/js/modules/loader.js
@@ -2,11 +2,9 @@ var loader = (function() {
   var ldr = document.getElementById('loader');
 
   Pace.on('hide', function() {
-    setTimeout(function() {
-      ldr.classList.add('fadeOut');
+    ldr.classList.add('fadeOut');
       setTimeout(function() {
         ldr.classList.add('hidden');
       }, 500);
-    }, 500);
   });
 })();
