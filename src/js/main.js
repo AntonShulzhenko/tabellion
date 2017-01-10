56b7d7101ad0171357d0e022c26b30ada2788f82
Anton SHulzhenko <shulzhenkomailbox@gmail.com>
2017-01-10 18:41:24 +0200

    edited services section

--- src/js/main.js
+++ src/js/main.js
@@ -119,35 +119,39 @@ function initMap() {
 })(jQuery);
 
 (function() {
-  var mo            = document.querySelector('.mobile-overlay');
-  var header        = document.querySelector('.header');
-  var closeBtn      = document.querySelector('.mobile-overlay__close');
-  var mobilePones   = document.querySelector('.mobile-overlay__phones');
-  var mobileAddress = document.querySelector('.mobile-overlay__address');
-  var phoneNumber   = document.querySelector('.header .contacts__item_phone').textContent;
-  var location      = document.querySelector('.header .contacts__item_location').textContent;
+  var mo          = document.querySelector('.mobile-overlay');
+  var header      = document.querySelector('.header');
+  var closeBtn    = document.querySelector('.mobile-overlay__close');
+  var mobileContent = document.querySelector('.mobile-overlay__content');
+  var phoneNumber = document.querySelector('.header .contacts__item_phone').textContent;
+  var location    = document.querySelector('.header .contacts__item_location').textContent;
+  var email       = document.querySelector('.header .contacts__item_email').textContent;
 
   if(window.innerWidth < 900) {
-    mobilePones.setAttribute('href', 'tel:' + phoneNumber);
-    mobilePones.innerHTML = phoneNumber;
-    mobileAddress.innerHTML = location;
-
     header.addEventListener('click', function(e) {
-      if(e.target.classList.contains('contacts__item_phone')) {
+      if (e.target.classList.contains('contacts__item')) {
         mo.classList.add('is-active');
-        mobileAddress.style.display = 'none';
-        mobilePones.style.display = 'block';
-        mobilePones.style.opacity = 1;
-      } else if(e.target.classList.contains('contacts__item_location')) {
-        mo.classList.add('is-active');
-        mobilePones.style.display = 'none';
-        mobileAddress.style.display = 'block';
-        mobileAddress.style.opacity = 1;
+        mobileContent.style.opacity = 1;
+        mobileContent.setAttribute('href', 'javascript:void(0);');
+
+        if(e.target.classList.contains('contacts__item_phone')) {
+          mobileContent.textContent = '';
+          mobileContent.textContent = phoneNumber;
+          mobileContent.setAttribute('href', 'tel:' + phoneNumber);
+        } else if(e.target.classList.contains('contacts__item_location')) {
+          mobileContent.textContent = '';
+          mobileContent.textContent = location;
+        } else {
+          mobileContent.textContent = '';
+          mobileContent.textContent = email;
+        }
       }
     });
 
     closeBtn.addEventListener('click', function(e) {
       mo.classList.remove('is-active');
+      mobileContent.textContent = '';
+      mobileContent.setAttribute('href', '');
     });
   }
 })();
