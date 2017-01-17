c78c437ad6d6f7ae4fea65d2bdd47f86dc7a2eab
Anton SHulzhenko <shulzhenkomailbox@gmail.com>
2017-01-17 21:44:09 +0200

    edited

--- src/js/modules/mobile.js
+++ src/js/modules/mobile.js
@@ -1,37 +1,46 @@
 (function() {
-  var mo          = document.querySelector('.mobile-overlay');
-  var header      = document.querySelector('.header');
-  var closeBtn    = document.querySelector('.mobile-overlay__close');
+  var mo            = document.querySelector('.mobile-overlay');
+  var closeBtn      = document.querySelector('.mobile-overlay__close');
   var mobileContent = document.querySelector('.mobile-overlay__content');
-  var phoneNumber = document.querySelector('.header .contacts__item_phone').textContent;
-  var location    = document.querySelector('.header .contacts__item_location').textContent;
-  var email       = document.querySelector('.header .contacts__item_email').textContent;
+  var header        = document.querySelector('.header');
+  var phoneNumber   = header.querySelector('.contacts__item_phone').textContent;
+  var location      = header.querySelector('.contacts__item_location').textContent;
+  var email         = header.querySelector('.contacts__item_email').textContent;
+
+  function toggleOverlay() {
+    mo.classList.toggle('is-active');
+  }
+
+  function showContent(el, attr, text) {
+    el.style.opacity = 1;
+    el.setAttribute('href', attr);
+    el.textContent = text;
+  }
+
+  function hideContent() {
+    mobileContent.innerHTML = '';
+    mobileContent.style.opacity = 0;
+    mobileContent.setAttribute('href', '');
+  }
 
   if(window.innerWidth < 900) {
     header.addEventListener('click', function(e) {
-      if (e.target.classList.contains('contacts__item')) {
-        mo.classList.add('is-active');
-        mobileContent.style.opacity = 1;
-        mobileContent.setAttribute('href', 'javascript:void(0);');
+      if(!e.target.classList.contains('contacts__item')) return;
 
-        if(e.target.classList.contains('contacts__item_phone')) {
-          mobileContent.textContent = '';
-          mobileContent.textContent = phoneNumber;
-          mobileContent.setAttribute('href', 'tel:' + phoneNumber);
-        } else if(e.target.classList.contains('contacts__item_location')) {
-          mobileContent.textContent = '';
-          mobileContent.textContent = location;
-        } else {
-          mobileContent.textContent = '';
-          mobileContent.textContent = email;
-        }
+      toggleOverlay();
+      
+      if(e.target.classList.contains('contacts__item_email')) {
+        showContent(mobileContent, 'mailto:' + email, email);
+      } else if(e.target.classList.contains('contacts__item_phone')) {
+        showContent(mobileContent, 'tel:' + phoneNumber, phoneNumber);
+      } else if(e.target.classList.contains('contacts__item_location')) {
+        showContent(mobileContent, 'http://maps.google.com?q=' + location, location);
       }
     });
 
     closeBtn.addEventListener('click', function(e) {
-      mo.classList.remove('is-active');
-      mobileContent.textContent = '';
-      mobileContent.setAttribute('href', '');
+      toggleOverlay();
+      hideContent();
     });
   }
 })();
